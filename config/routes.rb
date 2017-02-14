98397e07718d43b638bc7c6a5e13b4c79ad3778a
paul manot <paulmanot@gmail.com>
2017-02-14 17:06:36 +0100

    finish exo 1

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,19 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  resources :tasks
+
+  # # read all tasks
+  # get 'tasks' => 'tasks#index'
+  # # read a specific task
+  # get 'tasks/:id' => 'tasks#show'
+  # # create a new task
+  # get 'tasks/new' => 'tasks#new'
+  # post 'tasks' => 'tasks#create'
+  # # update a task
+  # get 'tasks/:id/edit' => 'tasks#edit'
+  # patch 'tasks/:id' => 'tasks#update'
+  # # destroy a task
+  # delete 'tasks/:id' => 'tasks#destroy'
+
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170214110120) do
+ActiveRecord::Schema.define(version: 20170214111931) do
 
   create_table "tasks", force: :cascade do |t|
     t.string   "name"
     t.boolean  "done"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+    t.datetime "created_at",  null: false
+    t.datetime "updated_at",  null: false
+    t.text     "description"
+    t.integer  "priority"
   end
 
 end
